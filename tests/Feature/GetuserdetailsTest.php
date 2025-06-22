2f37552efb8d2c04d6d71aad9af56a43c6c305ba
Kavindu Methmal <kavindumethmal1234gmail.com>
2025-06-22 13:56:46 +0530

    write frist test case

--- tests/Feature/GetuserdetailsTest.php
+++ tests/Feature/GetuserdetailsTest.php
@@ -10,6 +10,18 @@ final class GetuserdetailsTest extends TestCase
     //first test case
     public function test_get_user_details_when_calling_api_endpoint()
     {
+        //AAA
+        //A - Arrenge - not use actual data and use dummy data for laravel factory
+        $user = [
+            'user_name' => 'kavindu',
+            'email' => 'kavindugmail.com',
+            'user_role' => 'admin',
+        ];
+        //A - Act/Acton(This need to call you testing action)
+        $response = $this->get('api/get-users');
+
+        //A - Assertion (compare)
+        $response->assertStatus(200);
 
     }
 

26d1d68f5420e3e779f8f651db7096eae609577b
Kavindu Methmal <kavindumethmal1234gmail.com>
2025-06-22 11:11:01 +0530

    Inizilled fresh project

--- routes/api.php
+++ routes/api.php
@@ -4,5 +4,5 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/test', function () {
-    return 'test';
+    return  'test';
 });
---
+++ tests/Feature/GetuserdetailsTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Tests\Feature;
+
+
+use Tests\TestCase;
+
+final class GetuserdetailsTest extends TestCase
+{
+    //first test case
+    public function test_get_user_details_when_calling_api_endpoint()
+    {
+
+    }
+
+}

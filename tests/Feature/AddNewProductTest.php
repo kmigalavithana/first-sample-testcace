eeb62d44818682e6d94cce5cfb0b3b7d46a9c024
Kavindu Methmal <kavindumethmal1234gmail.com>
2025-06-24 22:14:04 +0530

    Create first test case.unit

---
+++ tests/Feature/AddNewProductTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature;
+
+
+
+use App\Models\Product;
+use Tests\TestCase;
+
+class AddNewProductTest extends TestCase
+{
+    public function test_add_new_product(): void
+    {
+
+    }
+
+}

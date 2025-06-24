362604ccb6aad368e355d6698fdb57e6943bc911
Kavindu Methmal <kavindumethmal1234gmail.com>
2025-06-24 22:25:00 +0530

    Run test ok so test db mde in dummy data it ok.but we are enyway run test prent we use refreshdb.

--- tests/Feature/AddNewProductTest.php
+++ tests/Feature/AddNewProductTest.php
@@ -5,13 +5,18 @@ namespace Tests\Feature;
 
 
 use App\Models\Product;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
 class AddNewProductTest extends TestCase
 {
+    use RefreshDatabase;
     public function test_add_new_product(): void
     {
+        ///AAA
 
+        ///A- Arrange
+        product::factory()->create();
     }
 
 }

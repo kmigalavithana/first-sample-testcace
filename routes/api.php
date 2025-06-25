ff8f28e3aa0cf00e16eae5e813bdfe124bba0333
Kavindu Methmal <kavindumethmal1234gmail.com>
2025-06-25 20:10:13 +0530

    first test case pass Refactor succuefully.

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,10 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/get-users',[UserController::class,'getUsers']);
+
+Route::post('create-product',function (Request $request){
+   return response()->json([
+       'status'=>200,
+       'message'=>'Product created successfully',
+   ]);
+});
--- tests/Feature/AddNewProductTest.php
+++ tests/Feature/AddNewProductTest.php
@@ -16,7 +16,18 @@ class AddNewProductTest extends TestCase
         ///AAA
 
         ///A- Arrange
-        product::factory()->create();
+        $product = Product::factory()->make();
+
+
+        ///A-Act
+        $response = $this->post('api/create-product', [$product]);
+
+        ///A-Assention
+        $response->assertStatus(200);
+        $response->assertJsonStructure([
+            'status',
+            'message',
+        ]);
     }
 
 }
